65b0d46ddc187b972dbb2ccd1c85ecfcd1eb94c7
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-24 06:41:21 +0000

    add mnemonic
    ディスプレイの濃淡が表示されるバグがある．

---
+++ bit.s
@@ -0,0 +1,25 @@
+@ Free & Reserved Register
+@ X {r0} : TIMER_BASE address.
+@ X {r5} : 更新目標時刻
+@ O {r6} : Current time 呼び出しに使う[何入れてもOK]
+@ X {r9} : bit_bufferが何巡目か(0 <= r9 <= 8)
+@ O {r10}: temp
+@ X {r11}: bit_buffer header address.
+@ X {r12}: bit_b
+	.include	"common.h"
+	.section	.text
+	.global		bit
+bit:
+	push	{r14}
+	ldr		r6,		[r0, #GPFSEL1]
+	cmp		r5,		r6
+	bcc		endp
+	ldr		r11,	=bit_buffer
+	ldr		r12,	=frame_buffer
+	ldrb	r10,	[r10, r9]
+	mov		r6,		#1
+	bl		clear
+	strb	r10,	[r12, r6]
+endp:
+	pop		{r14}
+	bx		r14
