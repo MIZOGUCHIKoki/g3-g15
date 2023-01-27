bd50c6aa554a3fc91905bc50926bc24116703f57
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-01-27 07:40:57 +0000

    Revert "clearのLED操作を削除"
    This reverts commit e5911d29ed47808d01d68ede0d98711c92540636.

--- result.s
+++ result.s
@@ -1,29 +1,52 @@
-@ Free & Reserved Register
-@ 0 {r0} : Free
-@ 0 {r1} : Free
-@ 0 {r2} : Free
-@ 0 {r3} : Free
-@ 0 {r4} : Free
-@ 0 {r5} : Free
-@ 0 {r6} : Free
-@ X {r7} : 現在のスコアを管理
-@ X {r8} : OK Flag
-@ 0 {r9} : Free
-@ 0 {r10} : frame_buffer
-@ 0 {r11} : frame_bufferに書き込みに使用
-@ 0 {r12} : Free
+@
+@ {r0} :
+@ {r1} :
+@ {r2} :
+@ {r3} :
+@ {r4} :
+@ {r5} :
+@ {r6} :
+@ {r7} :
+@ {r8} :
+@ {r9} :
+@ {r10} :
+@ {r11} :
+@ {r12} :
 	.include "common.h"
 	@成功したら(A)を点滅させる
 	@失敗したら(A)の一つのLEDを消す
+	.equ	TIMER_HZ,   100000    
 	.section .text
 	.global clear, miss
 
 clear:
+	push	{r2 - r4,r14}
+	ldr	r6, =TIMER_HZ
+	ldr	r10, [r0, #GPFSEL1]		@点灯タスク目標時刻 (現在時刻)
+	add	r11, r10, r6			@消灯タスク目標時刻
+	mov	r1,  #(1 << LED_PORT)	@条件を満たした場合行う
+	
 	@Ok Flagを1にする
 	mov 	r8,	#1
-	
-	bx	r14
+	ldr	r2,	=GPIO_BASE
+	mov	r3,	#2
+loop:
+	ldr	r4,	=0xff
+	@LEDを0.1秒周期で点滅三回させる
+	strne	r1, [r2, #GPSET0]	@1であれば点灯
+loop0:	
+	subs	r4,	r4,	#1
+	bne	loop0
+	streq	r1, [r2, #GPCLR0]	@0であれば消灯
+	ldr	r4,	=0xff
+loop1:
+	subs	r4,	r4,	#1
+	bne	loop1
+	subs	r3,	r3,	#1
+	bne	loop
 
+	pop	{r2 - r4,r14}
+	bx	r14
 
 miss:
 	@条件を満たさなかった場合行う
