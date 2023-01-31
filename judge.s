9eb32cb99ec6d6f8741dee1ab85a58dba834c1b8
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2023-01-31 14:42:15 +0900

    add led_on

--- judge.s
+++ judge.s
@@ -5,14 +5,13 @@ judge:
 	push	{r0-r12, r14}
 
 	ldr	r0,	=frame_buffer
-	ldrb	r2,	[r0, #7]
+	ldrb	r11,	[r0, #7]
 	bl	read_switch		@r1
 
-	ldr	r0,	=TIMER_BASE
-	ldr	r3,	=100000
-	ldr	r4,	[r0, #GPFSEL1]
-	add	r5,	r4,	r3
-	mov	r7,	#0
+	ldr	r4,	=100000
+	ldr	r5,	[r2]		@start_time
+	add	r3,	r5,	r4	@led_on
+	mov	r8,	#0
 	
 	cmp	r8,	#0
 	bxne	r14
@@ -20,38 +19,36 @@ judge:
 if1:	
 	cmp	r1,	#1
 	bne	if2
-	tst	r2,	#224	@11100000
+	tst	r11,	#224	@11100000
 	bne	jumpclear
 	b	jumpmiss
 
 if2:
 	cmp	r1,	#2
 	bne	if3
-	tst	r2,	#7	@00000111
+	tst	r11,	#7	@00000111
 	bne	jumpclear
 	b	jumpmiss
 	
 if3:
-	tst	r2,	#224
+	tst	r11,	#224
 	beq	end
-	tst	r2,	#7
+	tst	r11,	#7
 	beq	end
 	b	jumpmiss
 
 jumpclear:
 	bl	clear
-	ldr	r6,	[r0, #GPFSEL1]
-	cmp	r4,	r6
-	addcc	r4,	r4,	r3
-	addcc	r7,	r7,	#1
+	ldr	r7,	[r2]
+	cmp	r5,	r7
+	addcc	r5,	r5,	r4
 	blcc	led_on
-
-	cmp	r5,	r6
-	addcc	r5,	r5,	r3
+	cmp	r3,	r7
+	addcc	r3,	r3,	r4
 	blcc	led_off
-	cmp	r7,	#3
-	bne 	jumpclear
-	
+	add	r8,	r8,	#1
+	cmp	r8,	#3
+	bne	jumpclear
 	b	end
 jumpmiss:
 	bl	miss
