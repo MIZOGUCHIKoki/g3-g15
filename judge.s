b0287372d46d88cabdc7dbc995cdc39a445326d7
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2023-01-31 15:05:09 +0900

    debug led

--- judge.s
+++ judge.s
@@ -8,11 +8,6 @@ judge:
 	ldrb	r11,	[r0, #7]
 	bl	read_switch		@r1
 
-	ldr	r4,	=100000
-	ldr	r5,	[r2]		@start_time
-	add	r3,	r5,	r4	@led_on
-	mov	r8,	#0
-	
 	cmp	r8,	#0
 	bxne	r14
 
@@ -39,16 +34,7 @@ if3:
 
 jumpclear:
 	bl	clear
-	ldr	r7,	[r2]
-	cmp	r5,	r7
-	addcc	r5,	r5,	r4
-	blcc	led_on
-	cmp	r3,	r7
-	addcc	r3,	r3,	r4
-	blcc	led_off
-	add	r8,	r8,	#1
-	cmp	r8,	#3
-	bne	jumpclear
+	bl	led_on
 	b	end
 jumpmiss:
 	bl	miss
