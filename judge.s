756c7ca1892f46ed3bda193fca6183a049b8c885
MIKAMI shu <250372y@ugs.kochi-tech.ac.jp>
2023-02-02 16:34:34 +0900

    test led_on and led_off

--- judge.s
+++ judge.s
@@ -1,15 +1,30 @@
+@ Free & Reserved Register
+@ 0 {r0} : Free
+@ 0 {r1} : Free
+@ 0 {r2} : Free
+@ 0 {r3} : Free
+@ 0 {r4} : Free
+@ 0 {r5} : Free
+@ 0 {r6} : Free
+@ X {r7} : 現在のスコアを管理
+@ X {r8} : OK Flag
+@ 0 {r9} : Free
+@ 0 {r10} : Miss Flag
+@ 0 {r11} : 八行目読み出し
+@ 0 {r12} : =frame_buffer
+	
 	.include	"common.h"
 	.section	.text
 	.global		judge
 judge:
-	push	{r0-r12, r14}
+	push	{r14}
 
-	ldr	r0,	=frame_buffer
-	ldrb	r11,	[r0, #7]
+	ldr	r12,	=frame_buffer
+	ldrb	r11,	[r12, #7]
 	bl	read_switch		@r1
 
 	cmp	r8,	#0
-	bxne	r14
+	bxne	clear_led
 
 if1:	
 	cmp	r1,	#1
@@ -34,15 +49,52 @@ if3:
 
 jumpclear:
 	bl	clear
-	bl	led_on
+	ldr	r12,	[r0, #GPFSEL1]
+	ldr	r11,	=100000
+	str	r12,	[r2, #4]
+	add	r12,	r12,	r11
+	str	r12,	[r2, #8]
+	
 	b	end
 jumpmiss:
 	bl	miss
 	b	end
 
+clear_led:
+	ldr	r6,	=count
+	ldrb	r5,	[r6]
+	ldr	r11,	=200000	
+	ldr	r12,	[r0, #GPFSEL1]		@current_time
+	ldr	r3,	[r2, #4]		@led_on_time
+	cmp	r3,	r12
+	blcc	led_on
+	addcc	r3,	r3,	r11
+	strcc	r3,	[r2, #4]
+	addcc	r5,	r5,	#1
+	strcc	r5,	[r6]	
+
+	ldr	r4,	[r2, #8]
+	cmp	r4,	r12
+	blcc	led_off
+	addcc	r4,	r4,	r11
+	strcc	r4,	[r2, #8]
+	addcc	r5,	r5,	#1
+	strcc	r5,	[r6]
+
+	cmp	r5,	#6
+	moveq	r5,	#0
+	streq	r5,	[r6]
+	
+	
 end:
-	pop	{r0-r12, r14}
+	
+	pop	{r14}
 	bx	r14
+
+	
+	.section	.data
+count:
+	.byte	0
 	
 
 	
