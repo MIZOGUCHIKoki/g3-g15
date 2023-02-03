ee05aff94816d812c43bcc37d7b8ce1c4e5e7bfe
1250382KoukiYamada <250382g@ugs.kochi-tech.ac.jp>
2023-02-03 15:41:56 +0900

    gameover作り直し 新sound.sを利用した

--- gameover.s
+++ gameover.s
@@ -1,78 +1,82 @@
-	.include	"common.h"
-	.section	.text
-	.global		gameover
+	.include "common.h"
+	.section .text
+	.global gameover
 gameover:
-	push	{r0-r12, r14}
+	push  {r0-r12,r14}
+	bl    settings
+
+	ldr r0 , =TIMER_BASE  @変更しない
+	ldr r12, =PWM_BASE    @変更しない
+	ldr r1 , =time_2       @目標時刻いじる際に使用
+	ldr r2 , [r0, #GPFSEL1]@現在時刻
+	ldr r3 , =melody_2      @変更しない
+	ldr r4 , =rhythm_2      @変更しない
+	ldr r5 , =sound_data  @変更しない
+	ldr r6 , =rhythm_data @変更しない
+	ldr r7 , =count_2     @変更しない
+	@using register --r0,r1,r2,r3,r4,r5,r6,r7
+	@r8は目標時刻の更新に使用する
+	@r9鳴らしたい音を持つレジスタ
+	@r10鳴らしたいリズムを持つレジスタ
+	@r11countの数ldr,strする用
+
+	ldr r8 , [r1]   @音を鳴らす目標時刻をldr
+	@音を鳴らす時刻かどうかを確認
+	cmp r2 , r8     @r2のが大の時,音を鳴らす
+	bcc		mute
+	ldr r11 , [r7] 
+	ldr r9 , [r3, r11, lsl #2]  
+	ldr r10, [r4, r11, lsl #2]
+	ldr r9 , [r5, r9 , lsl #2]
+	ldr r10, [r6, r10, lsl #2]
+	add r11, r11, #1  @count更新
+	cmp	r11, #17
+	moveq r11, #0
+	str r11, [r7]     @count更新後データstr
 	
-	bl	settings
+	@音を鳴らす
+	str r9 , [r12, #PWM_RNG2]
+	lsr r9 , r9, #1   @デゥーティー比を1/2
+	str r9 , [r12, #PWM_DAT2]
+	@r0-r8,r10,r12は変更禁止 
+	add r9 , r8, r10
+	str r9 , [r1, #0]   @音符の長さ
+	mov r10, r10, lsr #1@実際になっている時間 
+	add r9 , r8, r10    
+	str r9 , [r1, #4]   
 
-	ldr	r0,	=TIMER_BASE
-	
-	ldr	r2,	=soundData_2
-	ldr	r3,	=sound_longData_2
-	ldr	r4,	=sound2_2
-	ldr	r5,	=sound_long_2
+mute:
+	@r0-r7,r12
+	ldr   r8 , [r1, #4]   @消す時刻かどうか
+	ldr   r2 , [r0, #GPFSEL1]
+	cmp   r2 , r8   @r2のが大きい時音を消す
+	movcs r9 , #0
+	strcs r9 , [r12, #PWM_DAT2]
 
-	ldr	r7,	=count_2
-	ldrb	r6, 	[r7]
-
-	ldr	r12,	=PWM_BASE
-
-ON:
-	ldrb	r7,	[r4, r6]
-	ldrb	r8,	[r5, r6]		
-	ldr	r9,	[r2, r7, lsl #2]	@sound
-	ldr	r10,	[r3, r8, lsl #2]	@sound_long
-
-	cmp	r9, 	#0
-	streq	r9,	[r12, #PWM_DAT2]
-
-	strne	r9,	[r12, #PWM_RNG2]
-	lsrne	r9,	r9, 	#1
-	strne	r9,	[r12, #PWM_DAT2]
-
-	ldr	r7,	=time_2
-	ldr	r1,	[r7]
-	ldr	r11,	[r0, #GPFSEL1]
-	
-	cmp	r1,	r11
-	addcc	r1,	r1,	r10
-	strcc	r1,	[r7]
-	ldrcc	r1,	[r7, #4]
-	movcc	r8,	#0
-	strcc	r8,	[r12, #PWM_DAT2]
-
-	cmp	r1,	r11
-	addcc	r6,	r6,	#1
-	addcc	r1,	r1,	r10
-	strcc	r1,	[r7, #4]
-	cmp	r6,	#7
-	moveq	r6,	#0
-	ldr	r7,	=count_2
-	strb	r6,	[r7]
-
-	pop	{r0-r12, r14}
-	bx	r14
-	
-	
-
-
-	.section	.data
-	.global		soundData_2, sound_longData_2, time_2, sound2_2, sound_long_2, count_2
-	@シ4,ド5,レ5,ミ5,ファ5
-soundData_2:
-	.word	19472, 18355, 16354, 14567, 13753
-	
-sound_longData_2:
-	.word	700000, 230000, 120000, 350000
-
-time_2:
-	.word 	0x00, 0x00
-
-sound2_2:
-	.byte	0,4,4,4,3,2,1
-sound_long_2:
-	.byte	0,0,3,0,0,3,3
+	pop {r0-r12, r14}
+	bx  r14
+	.section .data
+	.global count_2,time_2
 count_2:
-	.byte	0
+	@次に鳴らしたい音.(melodyの数字)
+	@現在の鳴らしている音を外部から管理する用
+	.word 0
+time_2:
+	@音を鳴らす目標時刻,音を消す目標時刻
+	.word 0x0, 0x0
+melody_2:
 	@マリオが死んだ時の音楽：シ4,ファ5,ファ5,ファ5,ミ5,レ5,ド5
+	@鳴らしたい音を数字にする
+	.word 6,12,12,12,11,7
+rhythm_2:
+	@鳴らしたいリズムを数字にする
+	@符号
+	.word 1,1,2,1,1,2,2
+sound_data:
+	@音データ.下のファから上のミ
+	.word 27428, 25945, 24489, 23132, 21818, 200600, 19472, 18355, 17328, 16354, 15434, 14567, 13753, 12972
+	.word 12260, 11566, 10909, 10300, 9726, 9177, 8664, 8177, 7717, 7283
+rhythm_data:
+	@リズムデータ
+	@2分音符, 4分音符,  8分音符,16分音符,付点4分音符,付点8分音符
+	.word 1200000 ,600000   , 300000 , 150000 ,900000  ,450000 @ドラクエ
