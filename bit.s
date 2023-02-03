5450986ce0b96e26d17476f4502264337d691d01
MIZOGUCHI Koki <250373b@ugs.kochi-tech.ac.jp>
2023-02-03 06:26:33 +0000

    aplly gameover...
    there are bugs

--- bit.s
+++ bit.s
@@ -16,16 +16,14 @@ bit:
 	ldrb	r10,	[r11, r9]
 	mov		r6,		#1
 	strb	r10,	[r12, r6]
-endp:
 	pop		{r10}
 	bx		r14
 go_bit:
 	push	{r10}
-	ldr		r11,	=bit_buffer_2
+	ldr		r11,	=bit_buffer
 	ldr		r12,	=frame_buffer
 	ldrb	r10,	[r11, r9]
 	mov		r6,		#1
 	strb	r10,	[r12, r6]
-endp:
 	pop		{r10}
 	bx		r14
