20cc0cd8621ee37d094841c58b2265997453fcc3
1250382KoukiYamada <250382g@ugs.kochi-tech.ac.jp>
2023-02-06 15:37:49 +0900

    add function
    add reset system

--- settings.s
+++ settings.s
@@ -1,6 +1,6 @@
 	.include	"common.h"
 	.section	.text
-	.global		settings
+	.global		settings, reset
 settings:
 	ldr	r0,	=GPIO_BASE
 	ldr	r1, =GPFSEL_VEC0
@@ -28,3 +28,39 @@ settings:
 	str	r1,	[r0, #PWM_CTL]
 	
 	bx	r14
+reset:
+  @ Reset sound
+  ldr		r4,		=count
+  mov		r7,		#0
+  str		r7,		[r4]
+	ldr		r4,		=time
+	ldr		r7,		[r0, #GPFSEL1]
+	str		r7,		[r4]
+	@ Reset frequency
+	mov		r4,		#0
+	str		r4,		[r3, #20]
+  @ Reset display
+  ldr		r4,		=frame_init
+  ldr		r8,		=frame_buffer
+  ldr		r7,		[r4]
+  str		r7,		[r8]
+  ldr		r7,		[r4, #4]
+  str		r7,		[r8, #4]
+  @ Stop sound
+  ldr		r4,		=PWM_BASE
+  mov		r7,		#0
+  str		r7,		[r4, #PWM_DAT2]
+
+  mov		r4,		#0						@ display_row
+  mov		r7,		#8						@ 現在スコア
+  mov		r8,		#0						@ OK Flag
+  mov		r9,		#0						@ bit_buffer進捗管理
+  @	display_low 初期目標時刻設定
+  ldr		r6,		[r0, #GPFSEL1]
+  mov		r1,		#dpr
+  add		r6,		r6,	r1
+  str		r6,		[r2]
+  @ 更新目標時刻
+  ldr		r6,		[r0, #GPFSEL1]
+  ldr		r1,		[r3]		@ from frequency
+  add		r5,		r6,	r1	@ set target time
